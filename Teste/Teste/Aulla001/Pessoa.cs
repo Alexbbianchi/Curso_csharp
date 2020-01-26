e491ab7fabeed3734fdb0ac2321dd47f5a7b9378
Jessika Suniga <jessikasuniga@gmail.com>
2020-01-26 11:20:44 -0300

    Docker

---
+++ Teste/Teste/Aulla001/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste.Aulla001
+{
+    public class Pessoa 
+    {
+        private string nome;
+        private double altura;
+
+        public void setNome(String _nome)
+        {
+            this.nome = _nome;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        
+        public void setAltura(Double _altura)
+        {
+            this.altura = _altura;
+        }
+        public double getAltura()
+        {
+            return this.altura;
+        }
+
+        public void faleSobreVoce()
+        {
+            Console.WriteLine(
+                "Meu nome é " 
+                + nome + 
+                " e minha altura é de "
+                + altura
+            );
+        }
+
+        
+    }
+}
--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -3,11 +3,11 @@ using Teste.Aulla001;
 
 namespace Teste
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            Caneta c1 = new Caneta();
+            /*Caneta c1 = new Caneta();
             c1.Id = new Guid();
             c1.cor = "Azul";
             c1.ponta = 0.5f;
@@ -24,7 +24,29 @@ namespace Teste
             c2.ponta = 0.9f;
             c2.tampar();
             c2.status();
-            c2.rabiscar();
+            c2.rabiscar();*/
+
+            // teste
+            
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+            Pessoa pessoa3 = new Pessoa();
+
+            Console.WriteLine("Efetua a leitura dos dados três pessoas");
+            //Pessoa 1
+            Console.WriteLine("Dados da 1 pessoa");
+            Console.WriteLine("Informe o seu nome: ");
+
+            string entrada = Console.ReadLine();
+            pessoa1.setNome(entrada);
+
+            Console.WriteLine("Informe sua altura ");
+            entrada = Console.ReadLine();
+            pessoa1.setAltura(Convert.ToDouble(entrada));
+            pessoa1.faleSobreVoce();
+
+           
+
         }
     }
 }
