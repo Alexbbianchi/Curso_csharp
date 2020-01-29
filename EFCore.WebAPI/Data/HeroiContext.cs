66ab40b2840fb1ad9277fb4834eacb537869d615
Jessika Suniga <jessikasuniga@gmail.com>
2020-01-29 07:03:43 -0300

    BD

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+
+        public DbSet<Batalha> Batalhas { get; set; }
+
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=sa;Initial Catalog=HerroApp;Data Source=DESKTOP-2OV73Q7");
+        }
+
+        //https://www.youtube.com/watch?v=ipbSwv09dDU&t=196s
+    }
+}
